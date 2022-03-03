7a16ffe5ac403cd8b3e5d5a71491620dc25cf999
Jacob K <jacobknighj@gmail.com>
2022-03-03 15:52:00 +1100

    skeliton

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export function NavBar() {
+    return (
+        <div className='nav'>
+            <h1>Jacob Knight</h1>
+            <a>Home</a>
+            <a>About</a>
+            <a>Projects</a>
+            <a>Contact</a>
+        </div>
+    );
+}
---
+++ src/components/ProjectsPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export function ProjectsPage() {
+    return (
+        <div>
+            <h1>Projects Page</h1>
+        </div>
+    );
+}
